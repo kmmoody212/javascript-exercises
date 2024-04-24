b7c09294e4d43e3699e958406052511db0608819
Katrina Moody <137461461+kmmoody212@users.noreply.github.com>
2024-04-24 17:13:55 -0400

    Completed and passed all tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+  let finalSum = 0;
+  for (let i = min; i <= max; i++) {
+    finalSum += i;
+  }
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  else {
+    return finalSum;
+  }
 };
+
+console.log(sumAll(10, [90, 1]));
 
 // Do not edit below this line
 module.exports = sumAll;
