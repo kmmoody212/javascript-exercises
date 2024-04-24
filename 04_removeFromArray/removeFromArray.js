1009f27f6bf66404a4b476b286e1c115f7ef6a66
Katrina Moody <137461461+kmmoody212@users.noreply.github.com>
2024-04-24 15:28:03 -0400

    Completed assignment and passed all tests.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,40 @@
-const removeFromArray = function() {
+/*
+let arr = [1, 2, 3, 4];
+let removeFromArray = arr.indexOf(3);
+if (removeFromArray !== -1) {
+  arr.splice(removeFromArray, 1);
+}
 
+console.log(arr);
+*/
+
+//ANOTHER METHOD///////////////////////////////////
+/*
+const arr = [1, 2, 3, 4];
+
+const removeFromArray = arr.filter((item) => {
+  return item != 3;
+});
+
+console.log(removeFromArray);
+*/
+
+const removeFromArray = function (array, ...args) {
+  // create a new empty array
+  const newArray = [];
+  // use forEach to go through the array
+  array.forEach((item) => {
+    // push every element into the new array
+    // UNLESS it is included in the function arguments
+    // so we create a new array with every item, except those that should be removed
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  // and return that array
+  return newArray;
 };
 
-// Do not edit below this line
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
+
 module.exports = removeFromArray;
