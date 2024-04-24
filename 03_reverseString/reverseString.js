59f3e50bba407f5b146a6cb715c844400e226ce2
Katrina Moody <137461461+kmmoody212@users.noreply.github.com>
2024-04-24 13:38:18 -0400

    Completed and passed all tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+function reverseString(str) {
+  let splitString = str.split("");
+  let reverseArray = splitString.reverse();
+  let joinArray = reverseArray.join("");
+  return joinArray;
+}
 
-};
+reverseString("");
 
 // Do not edit below this line
 module.exports = reverseString;
