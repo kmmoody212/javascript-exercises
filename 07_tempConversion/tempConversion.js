d8056d67f00fb8eae3ab7534a110c387599f0f58
Katrina Moody <137461461+kmmoody212@users.noreply.github.com>
2024-04-25 17:05:03 -0400

    Completed and passed all tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let celsius = ((temp - 32) * 5) / 9;
+  //let roundedNumber = celsius.toFixed(1);
+  let withoutDecimal = Math.trunc(celsius);
+  return withoutDecimal;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let fahrenheit = (temp * 9) / 5 + 32;
+  //let roundedNumber = fahrenheit.toFixed(1);
+  let withoutDecimal = Math.trunc(fahrenheit);
+  return withoutDecimal;
 };
 
-// Do not edit below this line
+convertToCelsius(32);
+convertToFahrenheit(0);
+
+//Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
